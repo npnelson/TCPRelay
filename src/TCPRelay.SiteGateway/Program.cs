87525236de61841b498e3c322a614636eec0b7b5
npnelson <nnelson1@insight.rr.com>
2019-08-21 15:48:57 -0400

    wip

---
+++ src/TCPRelay.SiteGateway/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPRelay.SiteGateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
